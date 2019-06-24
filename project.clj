93fe0f229b85ab0933b0324de504a4f8a9be43f1
Yana Lebedeva <kollapse@gmail.com>
2019-06-24 19:14:20 -0400

    project.clj: order build deps alphabetically

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src"]
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.11.0"]
-                 [ring "1.4.0"]
-                 [org.apache.santuario/xmlsec "2.0.4"]
+  :dependencies [[clj-time "0.11.0"]
                  [compojure "1.5.0"]
-                 [org.opensaml/opensaml "2.6.4"]
-                 [org.clojure/data.xml "0.0.7"]
+                 [org.apache.santuario/xmlsec "2.0.4"]
+                 [org.clojure/clojure "1.8.0"]
                  [org.clojure/data.codec "0.1.0"]
+                 [org.clojure/data.xml "0.0.7"]
                  [org.clojure/data.zip "0.1.1"]
-                 [org.vlacs/helmsman "1.0.0-alpha5"]]
+                 [org.opensaml/opensaml "2.6.4"]
+                 [org.vlacs/helmsman "1.0.0-alpha5"]
+                 [ring "1.4.0"]]
   :pedantic :warn
   :profiles {:dev {:source-paths ["dev" "test"]
                    :dependencies [[org.clojure/tools.namespace "0.2.10"]
