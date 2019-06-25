f542d8d7f907ea6db4517c59b559f77260741f1c
Ming Xie <ming.xie@kirasystems.com>
2019-06-25 11:27:59 -0400

    Tagging 0.1.13 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kirasystems/saml20-clj "0.1.13-SNAPSHOT"
+(defproject kirasystems/saml20-clj "0.1.13"
   :description "Basic SAML 2.0 library for SSO."
   :url "https://github.com/k2n/saml20-clj"
   :license {:name "Eclipse Public License"
