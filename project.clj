7674afb221b206a42bc1ee28217fded4b55fb974
Dhruvil Patel <dhruvil.h.patel@gmail.com>
2019-12-03 13:04:50 -0700

    Bump dependencies for projects + move doc-builder attributes to project.clj (JDK11)

--- project.clj
+++ project.clj
@@ -4,20 +4,23 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src"]
-  :dependencies [[clj-time "0.11.0"]
-                 [compojure "1.5.0" :exclusions [ring/ring-core]] ;ring/ring-core is part of [ring] below
-                 [org.apache.santuario/xmlsec "2.0.4"]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.codec "0.1.0"]
-                 [org.clojure/data.xml "0.0.7"]
-                 [org.clojure/data.zip "0.1.1"]
+  :dependencies [[clj-time "0.15.2"]
+                 [compojure "1.6.1" :exclusions [ring/ring-core]]
+                 [org.apache.santuario/xmlsec "2.1.4"]
+                 [org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.codec "0.1.1"]
+                 [org.clojure/data.xml "0.0.8"]
+                 [org.clojure/data.zip "0.1.3"]
                  [org.opensaml/opensaml "2.6.4"]
-                 [org.vlacs/helmsman "1.0.0-alpha5"]
-                 [ring "1.4.0"]]
+                 [org.vlacs/helmsman "1.0.0"]]
+  :jvm-opts ["-Djdk.xml.entityExpansionLimit=2000"
+             "-Djdk.xml.totalEntitySizeLimit=100000"
+             "-Djdk.xml.maxParameterEntitySizeLimit=10000"
+             "-Djdk.xml.maxElementDepth=1000"]
   :pedantic :warn
   :profiles {:dev {:source-paths ["dev" "test"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.10"]
-                                  [org.clojure/tools.nrepl "0.2.3"]
+                   :dependencies [[org.clojure/tools.namespace "0.3.1"]
+                                  [org.clojure/tools.nrepl "0.2.13"]
                                   [hiccup "1.0.5"]
                                   [http-kit "2.3.0"]]}}
   :repositories [["releases" {:url "https://clojars.org/repo"
